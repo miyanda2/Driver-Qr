bbc3305ef52b1eb455bed7fbf493768b705ebbc2
Goodness Ezeokafor <gootech442@yahoo.com>
2019-12-15 00:14:11 +0100

    redesign

--- driver/models.py
+++ driver/models.py
@@ -14,7 +14,35 @@ class Driver(models.Model):
     r code 
     '''
     name_of_driver  = models.CharField(max_length=250, blank=True,null=True)
+    email_address = models.EmailField()
+    address = models.CharField(max_length=14,blank=True,null=True)
+    city = models.CharField(max_length=250, blank=True,null=True)
+    phone_number = models.CharField(max_length=14,blank=True,null=True)
+    date_of_birth=models.DateTimeField()
+    educational_background = models.CharField(max_length=250, blank=True,null=True)
+    state_of_origin = models.CharField(max_length=250, blank=True,null=True)
+    local_government =models.CharField(max_length=250, blank=True,null=True)
+    blood_group= models.CharField(max_length=250, blank=True,null=True)
+    height=models.CharField(max_length=250, blank=True,null=True)
+    any_form_of_disability = models.CharField(max_length=250, blank=True,null=True)
+    nationality = models.CharField(max_length=250, blank=True,null=True)
+    next_of_kin = models.CharField(max_length=250, blank=True,null=True)
+    next_of_kin_phone_number = models.CharField(max_length=250, blank=True,null=True)
     uploadedImage =  models.ImageField(upload_to = 'Upload/',blank=False,null=True)
+    previous_number =models.CharField(max_length=250, blank=True,null=True)
     date_added = models.DateTimeField(auto_now_add=True)
     date_updated= models.DateTimeField(auto_now=True)
+
+
+
+class FieldNumber(models.Model):
+    driver =models.ForeignKey(Driver,models.CASCADE)
+    state_of_issuance = models.CharField(max_length=250, blank=True,null=True)
+    class_of_liscense= models.CharField(max_length=250, blank=True,null=True)
+    number_of_replacement = models.CharField(max_length=250, blank=True,null=True)
+    license_of_number = models.CharField(max_length=250, blank=True,null=True)
+    chassess_number = models.CharField(max_length=250, blank=True,null=True)
+    plate_number =models.CharField(max_length=250, blank=True,null=True)
+    expiry_date = models.DateTimeField()
+    flag = models.BooleanField(default=False)
 
